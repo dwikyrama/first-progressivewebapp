bfb9619be8400434a94b4d9397b5c95931d8b1cc
dwikyrama <dwikyry@gmail.com>
2019-11-02 22:30:41 +0700

    Added event listener for nav elements

--- js/nav.js
+++ js/nav.js
@@ -9,10 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4) {
         if (this.status != 200) return;
- 
+   
         // Muat daftar tautan menu
         document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
           elm.innerHTML = xhttp.responseText;
+        });
+   
+        // Daftarkan event listener untuk setiap tautan menu
+        document.querySelectorAll(".sidenav a, .topnav a").forEach(function(elm) {
+          elm.addEventListener("click", function(event) {
+            // Tutup sidenav
+            var sidenav = document.querySelector(".sidenav");
+            M.Sidenav.getInstance(sidenav).close();
+   
+            // Muat konten halaman yang dipanggil
+            page = event.target.getAttribute("href").substr(1);
+            loadPage(page);
+          });
         });
       }
     };
